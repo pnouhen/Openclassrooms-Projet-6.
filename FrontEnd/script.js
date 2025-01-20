08d56e957ac725bc7830aa90f11fe66840bad6d3
pnouhen <pierre.nouhen@orange.fr>
2025-01-20 14:23:40 +0100

    La <div> se remplit automatiquement !

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,18 +1,21 @@
 //  API
 async function fetchProject() {
   const tableauProject = await fetch("http://localhost:5678/api/works");
-  const tableauProjectJson = tableauProject.json();
-  const article = tableauProjectJson[0]
+  const tableauProjectJson = await tableauProject.json();
+  const article = tableauProjectJson[0];
   console.log(tableauProjectJson);
-  for(i = 1; i < 12; i++ ){
+  for (i = 0; i < tableauProjectJson.length; i++) {
+    // Figure in Gallery
     const figure = document.createElement("figure");
-gallery.appendChild(figure);
-const img = document.createElement("img");
-figure.appendChild(img);
-img.src = tableauProjectJson.imageUrl;
-const figcaption = document.createElement("figcaption");
-figure.appendChild(figcaption);
-figcaption.innerText = tableauProjectJson.title
+    gallery.appendChild(figure);
+    // Image
+    const img = document.createElement("img");
+    figure.appendChild(img);
+    img.src = tableauProjectJson[i].imageUrl;
+    // Title
+    const figcaption = document.createElement("figcaption");
+    figure.appendChild(figcaption);
+    figcaption.innerText = tableauProjectJson[i].title;
   }
 }
 
