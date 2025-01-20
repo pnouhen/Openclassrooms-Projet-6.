e7dcf0c5664fe3fe10f34f9548833f229517f00b
pnouhen <pierre.nouhen@orange.fr>
2025-01-20 13:59:22 +0100

    Apparation des 11 balises <figures>

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,19 +1,19 @@
-// Constante contenu
-const figure = document.createElement("figure");
-gallery.appendChild(figure);
-const img = document.createElement("img");
-figure.appendChild(img);
-const figcaption = document.createElement("figcaption");
-figure.appendChild(figcaption);
-
 //  API
 async function fetchProject() {
   const tableauProject = await fetch("http://localhost:5678/api/works");
   const tableauProjectJson = tableauProject.json();
   const article = tableauProjectJson[0]
-  img.src = tableauProjectJson.imageUrl;
-figcaption.innerText = tableauProjectJson.title
   console.log(tableauProjectJson);
+  for(i = 1; i < 12; i++ ){
+    const figure = document.createElement("figure");
+gallery.appendChild(figure);
+const img = document.createElement("img");
+figure.appendChild(img);
+img.src = tableauProjectJson.imageUrl;
+const figcaption = document.createElement("figcaption");
+figure.appendChild(figcaption);
+figcaption.innerText = tableauProjectJson.title
+  }
 }
 
 fetchProject();
