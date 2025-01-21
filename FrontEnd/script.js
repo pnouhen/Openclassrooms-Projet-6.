1db0767047dac2e7bc1101846c60e662228e874d
pnouhen <pierre.nouhen@orange.fr>
2025-01-21 13:12:29 +0100

    Création div filtrer, le texte et 1 input

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,14 +1,14 @@
 // Faire le lien avec l'API/works
-async function apiWorks(){
+async function apiWorks() {
   const tableauWorks = await fetch("http://localhost:5678/api/works");
   const tableauWorksJson = await tableauWorks.json();
-  return tableauWorksJson 
+  return tableauWorksJson;
 }
 // Association de la div
-const gallery = document.getElementById('gallery')
+const gallery = document.getElementById("gallery");
 //  Récupération des travaux depuis le back-end
 async function projet() {
-  const projetTableau = await apiWorks()
+  const projetTableau = await apiWorks();
   for (i = 0; i < projetTableau.length; i++) {
     // Figure in Gallery
     const figure = document.createElement("figure");
@@ -21,11 +21,18 @@ async function projet() {
     const figcaption = document.createElement("figcaption");
     figure.appendChild(figcaption);
     figcaption.innerText = projetTableau[i].title;
-}
+  }
 }
 projet();
 
-// Réalisation du filtre des travaux : Ajout des filtres pour afficher les travaux par catégorie 
-async function projectFilter(params) {
-  
-}
+// Réalisation du filtre des travaux : Ajout des filtres pour afficher les travaux par catégorie
+const portfolio = document.getElementById("portfolio");
+portfolio.appendChild(gallery);
+const filter = document.createElement("div");
+portfolio.appendChild(filter);
+portfolio.insertBefore(filter, gallery);
+const filterText = document.createElement("p");
+filter.appendChild(filterText);
+filterText.textContent= "Filtres :"
+const input = document.createElement("input");
+filter.appendChild(input);
