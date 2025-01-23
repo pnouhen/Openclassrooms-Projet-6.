61987b044af66d9975e8b2db1c63188ba55d6f16
pnouhen <pierre.nouhen@orange.fr>
2025-01-23 15:52:06 +0100

    Tentative 1 pour avoir une reponse du serveur

--- FrontEnd/script-login.js
+++ FrontEnd/script-login.js
@@ -1,13 +1,24 @@
 // Authentification de l’utilisateur
 const formConnexion = document.getElementById("formConnexion");
 
-formConnexion.addEventListener("submit", function(event) {
+formConnexion.addEventListener("submit", async function(event) {
   event.preventDefault(); // Empêche la soumission par défaut du formulaire
 
   const login = {
     email: event.target.querySelector("[name=email]").value, 
     mdp: event.target.querySelector("[name=mdp]").value, 
   };
-
-  console.log(login); 
+  // Transformer l'objet JavaScript login en une chaîne de caractères au format JSON
+  const chargeUtile = JSON.stringify(login);
+  // Appel du fetch
+    const replogin = await fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: chargeUtile,
+    });
+// const reploginData = await replogin.json()
+  console.log(replogin.ok)
 });
