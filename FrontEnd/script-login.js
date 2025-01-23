a88c947c8a642ce5130b8c2d854e57f1fa2c5dae
pnouhen <pierre.nouhen@orange.fr>
2025-01-23 11:33:43 +0100

    Recommencent à 0 de la connexion du site - changement const

--- FrontEnd/script-login.js
+++ FrontEnd/script-login.js
@@ -1,32 +1,8 @@
 // Authentification de l’utilisateur
 
-// Daprès le cours
-function sendLogin() {
-  const buttonConnexion = document.getElementById("connexion");
-
-  buttonConnexion.addEventListener("submit", async function (event) {
+// D'après le cours
+  const formConnexion = document.getElementById("formConnexion");
+// console.log(buttonConnexion)
+formConnexion.addEventListener("click", async function (event) {
     event.preventDefault();
-    // Create object Login
-    const login = {
-      email: event.target.querySelector("[name=email]").value,
-      mdp: event.target.querySelector("[name=mdp]").value,
-    };
-    //Create utile charge JSON
-    const chargeUtile = JSON.stringify(login);
-    // Call function fetch
-    const replogin = await fetch("http://localhost:5678/api/users/login", {
-      method: "POST",
-      header: {
-        accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: chargeUtile,
-    });
-    if(replogin < 400){
-        const data = await replogin.json()
-        console.log("Victoire", data)
-    }
-  });
-}
-
-sendLogin();
+    console.log("ok")})
