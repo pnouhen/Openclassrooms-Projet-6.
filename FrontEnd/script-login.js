40f425fdb05ae4c7549b4a51e8c5d5bd10c94569
pnouhen <pierre.nouhen@orange.fr>
2025-01-24 13:27:24 +0100

    J'ai mis la modification du hearder sous forme de fonction pour me simplifier la lecture de la boucle if

--- FrontEnd/script-login.js
+++ FrontEnd/script-login.js
@@ -1,6 +1,34 @@
 // Authentification de l’utilisateur
 const formConnexion = document.getElementById("formConnexion");
 
+function modeEdition(){
+  // Ajout de la barre de Edition
+// Recuperation des données et des classes
+const header = document.getElementById("header");
+const h1 = document.getElementById("h1");
+const nav = document.getElementById("nav")
+// Création de divEdition
+const divEdition = document.createElement("div");
+header.appendChild(divEdition);
+divEdition.classList.add('headerEdition')
+const iconEdition = document.createElement("i")
+divEdition.appendChild(iconEdition)
+iconEdition.classList.add("fa-regular","fa-pen-to-square")
+const pEdition = document.createElement("p");
+divEdition.appendChild(pEdition);
+pEdition.textContent = "Mode édition"
+// Regroupement des balises h1 et nav
+const divH1Nav = document.createElement("div")
+header.appendChild(divH1Nav)
+divH1Nav.appendChild(h1)
+divH1Nav.appendChild(nav)
+divH1Nav.classList.add("divH1Nav")
+// Propriétés CSS
+header.style.display = "flex"
+header.style.flexDirection = "column"
+header.style.marginTop = 0
+}
+
 formConnexion.addEventListener("submit", async function(event) {
   event.preventDefault(); // Empêche la soumission par défaut du formulaire
 
@@ -26,34 +54,12 @@ const reploginData = await replogin.json()
     localStorage.setItem('authToken', reploginData.token);
     const storedToken = localStorage.getItem('authToken');
     console.log(storedToken);
+    modeEdition()
+    
   } else if (replogin.status === 401) {
     console.log("Non autorisé");
   } else {
     console.log("Erreur inconnue : " + replogin.status);
   }
 });
-// Ajout de la barre de Edition
-// Recuperation des données et des classes
-const header = document.getElementById("header");
-const h1 = document.getElementById("h1");
-const nav = document.getElementById("nav")
-// Création de divEdition
-const divEdition = document.createElement("div");
-header.appendChild(divEdition);
-divEdition.classList.add('headerEdition')
-const iconEdition = document.createElement("i")
-divEdition.appendChild(iconEdition)
-iconEdition.classList.add("fa-regular","fa-pen-to-square")
-const pEdition = document.createElement("p");
-divEdition.appendChild(pEdition);
-pEdition.textContent = "Mode édition"
-// Regroupement des balises h1 et nav
-const divH1Nav = document.createElement("div")
-header.appendChild(divH1Nav)
-divH1Nav.appendChild(h1)
-divH1Nav.appendChild(nav)
-divH1Nav.classList.add("divH1Nav")
-// Propriétés CSS
-header.style.display = "flex"
-header.style.flexDirection = "column"
-header.style.marginTop = 0
+
