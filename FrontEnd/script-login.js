94d75d2e67533d9834429d34a7fdbf2b994e911f
Pierre Nouhen <pierre.nouhen@orange.fr>
2025-01-30 09:39:25 +0100

    Modification si erreur de connexion avec un message d'alerte

--- FrontEnd/script-login.js
+++ FrontEnd/script-login.js
@@ -26,21 +26,6 @@ formConnexion.addEventListener("submit", async function (event) {
     const storedToken = localStorage.getItem("authToken");
     window.location.href = "./index.html";
   } else {
-    // Création de la constante pour faire apparaitre le message une fois
-    const erreurExistante = document.querySelector(".MessageErreur");
-    if (!erreurExistante) {
-      // Créer un nouveau message d'erreur
-      const erreur = document.createElement("p");
-      erreur.classList.add("MessageErreur"); // Ajouter une classe pour styliser l'erreur
-      erreur.innerHTML = "Email/Mot de passe incorrect <br> Veuillez réessayer";
-      // Ajouter l'erreur au formulaire, avant le champ mot de passe
-      const formConnexion = document.getElementById("formConnexion");
-      formConnexion.appendChild(erreur);
-      const mdpo = document.getElementById("mdpo");
-      formConnexion.insertBefore(erreur, mdpo);
-      // Modifier la marge si nécessaire
-      const connexion = document.getElementById("connexion");
-      connexion.style.marginBottom = "0px";
-    }
+    alert("Email/Mot de passe incorrect Veuillez réessayer");
   }
 });
